33f7f2aa123d62116bb7c2283ec4ff93d9668244
ashutoshvpujari1-pixel <ashutoshvpujari1@gmail.com>
2026-08-15 17:59:44 +0530

    Add new controller class

---
+++ src/main/java/com/pt/notification/controller/GreetAPIController.java
@@ -0,0 +1,21 @@
+package com.pt.notification.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pt.notification.NotificationApplication;
+
+@RestController
+public class GreetAPIController {
+	
+	static Logger logger = LoggerFactory.getLogger(NotificationApplication.class);
+
+    @GetMapping("/greet")
+    public String sayGoodMorning(@RequestParam(value = "name", defaultValue = "Ashutosh") String name) {
+    	logger.info("main method started..");
+        return String.format("Good Morning %s", name);
+    }
+}
